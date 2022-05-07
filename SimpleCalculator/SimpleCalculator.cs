8404f4646ed03048dcc93870ff119304f60c6ac3
Serhii Cherevatenko <cherevatenko.s@gmail.com>
2022-05-07 14:58:00 +0300

    Add project files.

---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        public static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+    }
+}
